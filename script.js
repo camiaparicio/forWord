b6789811b08ead91ec0fcdcc9cee919ca67f38fe
Camila <camilaaparicioo@hotmail.com>
2022-02-18 14:02:56 -0500

    modal update

--- script.js
+++ script.js
@@ -1,35 +1,28 @@
 
+// name & domain
+
+const name = document.getElementById('userName')
+const email = document.getElementById('userEmail')
+const form = document.getElementById('form')
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+})
+
 // open and close buttons
 
 const open = document.getElementById('open');
 const modalContainer = document.getElementById('modalContainer');
-const close = document.getElementById('close');
+// const close = document.getElementById('close');
 
 
-open.addEventListener('click', () => {
+open.addEventListener('submit', () => {
     modalContainer.classList.add('show');
 });
 
-close.addEventListener('click', () => {
-    modalContainer.classList.remove('show');
-});
+// close.addEventListener('click', () => {
+//     modalContainer.classList.remove('show');
+// });
 
-// name & domain
-
-// const name = document.getElementById('userName')
-// const email = document.getElementById('userEmail')
-// const form = document.getElementById('form')
-
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-
-
-// })
-
-
-// function onSubmit() {
-
-// }
-
-// console.log(name.value)
-// console.log(email.value)
