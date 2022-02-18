ea51c69cb35dc01d050c99e9c440d3c308d43a1d
Camila <camilaaparicioo@hotmail.com>
2022-02-18 13:10:49 -0500

    html done

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const name = document.getElementById('userName')
+const email = document.getElementById('userEmail')
+const form = document.getElementById('form')
+
+const modal = document.getElementById('modal');
+const openBtn = document.querySelector('openBtn')
+const closeBtn = document.querySelector('closeBtn')
+
+
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+
+
+// })
+
+
+// function onSubmit() {
+
+// }
+
+// console.log(name.value)
+// console.log(email.value)
